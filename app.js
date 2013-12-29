1c4ecb223c62a7cb1ce41ad21302cea680762726
diliprajan <diliprajan.gfx@gmail.com>
2013-12-29 18:58:34 -0500

    Fixed port in app.js

--- app.js
+++ app.js
@@ -9,4 +9,7 @@ app.get('/', function(request, response){
 	response.sendfile(__dirname + "/views/index.html");
 });
 
-app.listen(5000);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
